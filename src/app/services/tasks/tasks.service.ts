18855f32a11e48b0f117c312a9ed16c8996eb674
Mohammad Reza Ghasemi <mrghasemi1992@gmail.com>
2024-02-26 15:20:31 +0330

    feat: persist tasks on the browser local storage

--- src/app/services/tasks/tasks.service.ts
+++ src/app/services/tasks/tasks.service.ts
@@ -7,13 +7,28 @@ import { Task } from '../../interfaces';
 export class TasksService {
   tasks: Task[] = [];
 
-  constructor() {}
+  constructor() {
+    this.loadTasks();
+  }
 
   addTask(task: Task) {
     this.tasks.push(task);
+    this.saveTasks();
   }
 
   deleteTask(id: string) {
     this.tasks = this.tasks.filter((task) => task.id !== id);
+    this.saveTasks();
+  }
+
+  private loadTasks() {
+    const savedTasks = localStorage.getItem('tasks');
+    if (savedTasks) {
+      this.tasks = JSON.parse(savedTasks);
+    }
+  }
+
+  private saveTasks() {
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
   }
 }
