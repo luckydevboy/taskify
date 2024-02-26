2d10ba577893b472ee2a8c11f28d84f0f4e1ec26
Mohammad Reza Ghasemi <mrghasemi1992@gmail.com>
2024-02-26 09:58:06 +0330

    fix: add missed modal

---
+++ src/app/components/ui/modal/modal.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import { NgIf } from '@angular/common';
+
+@Component({
+  selector: 'app-modal',
+  standalone: true,
+  imports: [NgIf],
+  templateUrl: './modal.component.html',
+  styleUrl: './modal.component.css',
+})
+export class ModalComponent {
+  constructor(public sanitized: DomSanitizer) {}
+
+  @Input() header?: string = '';
+  @Input() body: string = '';
+  @Input() footer?: string = '';
+  @Input() id: string = '';
+}
