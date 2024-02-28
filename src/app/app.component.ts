f1d5f50b60654edaff09099818d804fa6c092f31
Mohammad Reza Ghasemi <mrghasemi1992@gmail.com>
2024-02-28 10:10:06 +0330

    feat: add hint when there are no tasks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import { initFlowbite } from 'flowbite';
 import { LayoutComponent } from './components/layout/layout.component';
 import { AddTaskComponent } from './components/add-task/add-task.component';
 import { TaskComponent } from './components/task/task.component';
-import { NgForOf } from '@angular/common';
+import { NgForOf, NgIf } from '@angular/common';
 import { ModalComponent } from './components/ui/modal/modal.component';
 import { TasksService } from './services/tasks/tasks.service';
 import { Task } from './interfaces';
@@ -20,6 +20,7 @@ import { Task } from './interfaces';
     TaskComponent,
     NgForOf,
     ModalComponent,
+    NgIf,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
