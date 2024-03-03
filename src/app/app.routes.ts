c9d96eda95a977239616ecd11f96d59e9cd96608
Mohammad Reza Ghasemi <mrghasemi1992@gmail.com>
2024-03-03 12:55:57 +0330

    feat: 404 page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { SettingsComponent } from './pages/settings/settings.component';
 import { HomeComponent } from './pages/home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'settings', component: SettingsComponent },
+  { path: '**', component: NotFoundComponent },
 ];
